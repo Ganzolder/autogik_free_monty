004fbf0dcb96845111db584dba30abf29105beb7
ganzolder <gnmsapr@gmail.com>
2023-04-26 11:25:57 +0700

    changed width of fields

--- interface.py
+++ interface.py
@@ -9,7 +9,7 @@ def interface():
     layout = [
         [sg.Text('Дата продажи'), sg.InputText(size=(10, 2), key='date')],
         [sg.Text('ФИО клиента'), sg.InputText(size=(40, 2), key='cutomer_name')],
-        [sg.Text('Телефон клиента'), sg.InputText(size=(20, 2), key='customer_number')],
+        [sg.Text('Телефон клиента'), sg.InputText(size=(30, 2), key='customer_number')],
         [sg.Text('Бренд шин'), sg.Combo(['pirelli', 'nokian', 'cordiant'], size=(20, 2), key='tire_brand')],
         [sg.Output(size=(88, 20))],
         [sg.Submit(), sg.Cancel()]
