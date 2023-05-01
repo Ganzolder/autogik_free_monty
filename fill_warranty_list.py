6c06a8c21a9199c22a19281f9191311da0cf59dc
ganzolder <gnmsapr@gmail.com>
2023-05-02 01:19:32 +0700

    changed theme to dark black1. added brands list to combo box from base.xlsx.
    added base parameters in models. trying to make depended list in model dropbox
    
    Interface fully working with popup generated code with 4th number that sign count of manager in list. For now after press ok prog making writing in file. controlling unfilled fields

--- fill_warranty_list.py
+++ fill_warranty_list.py
@@ -1,11 +1,23 @@
 from openpyxl import load_workbook
 from datetime import datetime
+from interface import interface
 
-date, customer_name, customer_number, tire_brand, tire_model, tire_size, car_model, license_plate, check_number, manager = \
-    '01/03/1986', 'Ленин ВИ', '89539106192', 'Pirelli', 'P1 Verde', '195/65 R15', 'Nissan Сube', 'К602ТМ42', '65496864565', 'Волобуев'
+# {'date': '02/05/2023', 'cutomer_name': 'Волобуев Алексей', 'customer_number': '89539106192',
+# 'tire_size_width': 215, 'tire_size_profile': 55, 'tire_size_diameter': 'R16',
+# 'tire_brand': 'Tunga', 'tire_model': 'Zodiak 2', 'car_model': 'nissan cube', 'license_plate': 'k602tm',
+# 'check_number': '123123', 'manager': 'Шараев'}
 
-def fill_warranty_list(date, customer_name, customer_number, tire_brand, tire_model,
-                       tire_size, car_model, license_plate, check_number, manager):
+user_data = interface()
+
+tire_size = f'{user_data["tire_size_width"]}/{user_data["tire_size_profile"]} {user_data["tire_size_diameter"]}'
+
+date, customer_name, customer_number, tire_brand, tire_model, car_model, license_plate, check_number, manager = user_data["date"],\
+    user_data["customer_name"], user_data["customer_number"], user_data["tire_brand"],\
+    user_data["tire_model"], user_data["car_model"], user_data["license_plate"],\
+    user_data["check_number"], user_data["manager"]
+
+
+def fill_warranty_list(date, customer_name, customer_number, tire_brand, tire_model, tire_size, car_model, license_plate, check_number, manager):
     # открываем список бшм и делаем запись в последнюю пустую строку
 
     time_stamp = datetime.now()
@@ -24,6 +36,5 @@ def fill_warranty_list(date, customer_name, customer_number, tire_brand, tire_mo
 
     wb_warranty_list.save(filename='base/warranty_list.xlsx')
 
+fill_warranty_list(date, customer_name, customer_number, tire_brand, tire_model, tire_size, car_model, license_plate, check_number, manager)
 
-fill_warranty_list(date, customer_name, customer_number, tire_brand, tire_model,
-                   tire_size, car_model, license_plate, check_number, manager)
--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import get_base_parameters
+import random
 #  date, customer_name, customer_number, tire_brand, tire_model,
 #  tire_size_width, tire_size_profile, tire_size_diameter, car_model, license_plate, check_number, manager
 #  https://www.udemy.com/course/pysimplegui/
@@ -17,11 +18,11 @@ for brand in range(len(base_parameters[4])):
 #diameter_conditions = base_parameters[6]
 
 def interface():
-    #depended_model = base_parameters[5]['Pirelli']
+
     sg.theme('Dark black1')
     layout = [
         [sg.Text('Дата продажи', size=(12, 1)), sg.InputText(size=(40, 1), key='date')],
-        [sg.Text('ФИО клиента', size=(12, 1)), sg.InputText(size=(40, 1), key='cutomer_name')],
+        [sg.Text('ФИО клиента', size=(12, 1)), sg.InputText(size=(40, 1), key='customer_name')],
         [sg.Text('Тел. клиента', size=(12, 1)), sg.InputText(size=(40, 1), key='customer_number')],
         [sg.Text('Ширина', size=(12, 1)), sg.Combo(base_parameters[1], size=(5, 10), key='tire_size_width'),
         sg.Text('Проф.', size=(5, 1)), sg.Combo(base_parameters[2], size=(5, 10), key='tire_size_profile'),
@@ -32,11 +33,15 @@ def interface():
         [sg.Text('Номер авто', size=(12, 1)), sg.InputText(size=(40, 10), key='license_plate')],
         [sg.Text('Номер чека', size=(12, 1)), sg.InputText(size=(40, 10), key='check_number')],
         [sg.Text('Менеджер', size=(12, 1)), sg.Combo(base_parameters[0], size=(38, 12), key='manager')],
-
+        #[sg.print("Class",size=(15, 1), font=("Verdana",11),text_color='Red',justification='right', sg.OutPut
         [sg.Submit('Зарегистрировать'), sg.Cancel('Отмена')]
     ]
     window = sg.Window('Анкета бесплатного шиномонтажа', layout)
-    while True:                             # The Event Loop
+
+
+    while True: # The Event Loop
+
+        unfilled_fields = 0
         event, values = window.read()
         # print(event, values) #debug
 
@@ -47,4 +52,23 @@ def interface():
             depended_model = models_dict[values['tire_brand']]
             window['tire_model'].update(values=depended_model)
 
-interface()
+        if event == 'Зарегистрировать':
+
+            for value in values.values():
+                if value == '':
+                    unfilled_fields += 1
+
+            if unfilled_fields > 0:
+                sg.popup_auto_close('Заполните все поля', auto_close_duration=2)
+
+            else:
+                brand_name_cropped = values["tire_brand"][:3].upper()
+                rand_first = random.randint(100, 999)
+                managers_key = base_parameters[0].index(values['manager']) + 1
+                rand_last = random.randint(10, 99)
+                sg.popup_scrolled(f'БШМ КОД - {brand_name_cropped}{rand_first}{managers_key}{rand_last}', title='Скопируй и вставь в комментарий к заказу в 1с',
+                                  font=("Verdana Bold", 24), text_color='red', size=(22, 2))
+                return values
+
+
+#print(interface())
